265c2cd63d04260348e752eebfc1c06d6b304227
huaibaomengxiang <764297325@qq.com>
2018-01-30 17:12:26 +0800

    自适应

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 //波浪
 var bg=document.querySelector(".myInfo");
+var cw=bg.offsetWidth
 var ch=bg.offsetHeight;
 var c=document.querySelector("#bg");
 var step=0
-c.width=300;
+c.width=cw;
 c.height=ch;
 var ctx=c.getContext("2d")
 function loop() {
@@ -13,10 +14,18 @@ function loop() {
     let right=Math.cos(Math.PI/180*step)*50
     ctx.beginPath()
     ctx.fillStyle="#7ce0ff"
-    ctx.moveTo(c.width/2+left,0)
-    ctx.bezierCurveTo(c.width/2+left-50,c.height/2,c.width/2+right+50,c.height/2,c.width/2+right,c.height)
-    ctx.lineTo(0,c.height)
-    ctx.lineTo(0,0)
+    if(window.innerWidth<970){
+        ctx.moveTo(0,c.height/2+left)
+        ctx.bezierCurveTo(c.width/2,c.height/2+left-50,c.width/2,c.height/2+right+50,c.width,c.height/2+right)
+        ctx.lineTo(c.width,c.height)
+        ctx.lineTo(0,c.height)
+    }else{
+        ctx.moveTo(c.width/2+left,0)
+        ctx.bezierCurveTo(c.width/2+left-50,c.height/2,c.width/2+right+50,c.height/2,c.width/2+right,c.height)
+        ctx.lineTo(0,c.height)
+        ctx.lineTo(0,0)
+    }
+
     ctx.closePath()
     ctx.fill()
     requestAnimationFrame(loop)
@@ -29,7 +38,10 @@ ac.height=window.innerHeight;
 var actx=ac.getContext('2d'),
     dots=[],
     lineDistance=80;
-mouse=window.utils.captureMouse(ac);
+if(window.innerWidth>=970){
+    mouse=window.utils.captureMouse(ac);
+}
+
 function Dot() {
     this.x=Math.random()*ac.width;
     this.y=Math.random()*ac.height;
@@ -114,10 +126,11 @@ $("li").click(function () {
 })
 
 
-
-
-
 window.onresize=function () {
+    cw=bg.offsetWidth;
+    ch=bg.offsetHeight;
+    c.width=cw;
+    c.height=ch;
     ac.width=window.innerWidth;
     ac.height=window.innerHeight;
 }
--- js/rem.js
+++ js/rem.js
@@ -1,23 +1,20 @@
-// 当页面加载之后
-window.onload=function  () {
-	// 常量 const
-	const designWidh=980;
-	const fontSize= 100;
-	function resizeFont(){
-		// 变量 var
-		var CW = document.documentElement.clientWidth;
-		
-		var ratio = CW/designWidh;
+// 常量 const
+const designWidh=1920;
+const fontSize= 100;
+function resizeFont(){
+    // 变量 var
+    var CW = document.documentElement.clientWidth;
 
-		var newFontSize=ratio<1?fontSize*ratio:100;
+    var ratio = CW/designWidh;
 
-		document.querySelector("html").style.fontSize= newFontSize + "px";
-		
-	}
-	// 执行resizeFont
-	resizeFont();
-	// 窗口大小改变时执行resizeFont
-	window.onresize= resizeFont;
-	window.addEventListener("orientation",resizeFont,false);
-            // 屏幕自动旋转得时候
+    var newFontSize=ratio<1?fontSize*ratio:100;
+
+    document.querySelector("html").style.fontSize= newFontSize + "px";
+
 }
+// 执行resizeFont
+resizeFont();
+// 窗口大小改变时执行resizeFont
+window.onresize= resizeFont;
+window.addEventListener("orientation",resizeFont,false);
+// 屏幕自动旋转得时候
